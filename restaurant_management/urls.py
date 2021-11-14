f053fc1df23f7182c15d8bbf6f10412d73a57f1b
darkness22 <dunga1k58bh@gmail.com>
2021-11-14 14:17:21 +0700

    test

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from restaurant_management import views
 
 urlpatterns = [
-    path("home/", views.home, name="home"),    
+    path("", views.home, name="home"),    
+    path("hoadon/<int:ma_hoa_don>/", views.hoadon, name = "hoadon"),
     path("take_away/", views.takeAway, name="take_away"),
     path("events/", views.events, name="events"),
     path("vip_member/", views.vipMember, name="vip_member"),
     path("statistics/", views.statistics, name="statistics"),
     path("setting/", views.setting, name="setting"),
-    path("", views.signin, name = "signin"),
+    path("login", views.signin, name = "signin"),
 ]
--- restaurant_management/views.py
+++ restaurant_management/views.py
@@ -8,7 +8,7 @@ from restaurant_management.models import *
 # Create your views here.
 
 
-def signin(request): 
+def signin(request):
     if request.method == 'POST':
         username = request.POST.get("username")
         password = request.POST.get("password")
@@ -27,25 +27,21 @@ def signin(request):
    
 
 def home(request):
-    bans = Ban.objects.all()
-    # hoa_don = None
-    # if (request.method == 'POST'):
-    #     if "chon_ban" in request.POST :
-    #         so_ban = request.POST.get(so_ban)
-    #         print(so_ban)
-    #         ban = Ban.objects.get(so_ban = so_ban)
-    #         ma_hoa_don = ban.get_mahoadon()
-    #         if (ma_hoa_don != None):
-    #             hoa_don = HoaDon.objects.get(ma_hoa_don = ma_hoa_don)
-    #     if "add_hoa_don" in request.POST :
-    #         a =0
-              
+    bans = Ban.objects.all()     
     return render(request, "management/home.html", 
                   {
                         'bans' : bans,
-                        # 'hoa_don': hoa_don
                   })
-
+    
+def hoadon(request , ma_hoa_don):
+    bans = Ban.objects.all()
+    hoadons = HoaDon.objects.get(ma_hoa_don = ma_hoa_don)
+    return render(request, "management/hoadon.html",
+                  {
+                      'bans' : bans,
+                      'hoadons':hoadon
+                  })
+    
 def takeAway(request):
     return render(request, "management/take_away.html")
 
