6328c28447f6fea01020c4ed2549973a77262cf5
HanHD <duchanho1211@gmail.com>
2021-11-18 21:33:34 +0700

    commit

--- restaurant_management/static/management/site.js
+++ restaurant_management/static/management/site.js
@@ -1,30 +1,15 @@
 
-function newFunction(ban_so, mahoadon){
-    document.getElementById("bill__name").innerHTML = "Hóa đơn của bàn "+ban_so;
-    if(mahoadon != undefined){
-        document.getElementById("bill__id").innerHTML = "Mã hóa đơn: "+mahoadon;
-        document.getElementById("create__bill").style.zIndex = '-1' ;
-        document.getElementById("pay__bill").style.zIndex = '1' ;
-        document.getElementById("add__meal").style.zIndex = '1' ; 
-    }else{
-        document.getElementById("bill__id").innerHTML = "Bàn "+ ban_so+ " chưa có hóa đơn" ;
-        document.getElementById("create__bill").style.zIndex = '1' ;
-        document.getElementById("add__meal").style.zIndex = '-1' ; 
-        document.getElementById("pay__bill").style.zIndex = '-1' ; 
-    }
-
-}
-function addMealFunction() {
-    alert("Thêm món cho hóa đơn");
-    // var retVal = confirm("Do you want to continue ?");
+function deleteMeal(){
 
 }
 
-function payBillFunction(mahoadon){
-    alert("Thanh toán cho hóa đơn"+ mahoadon);
-    // var retVal = confirm("Do you want to continue ?");
-
+function addMealFunction(tenmon, giamon) {
+    alert("Thêm món "+tenmon+" cho hóa đơn");
+    var html=' <tr >  <th scope="row">  </th>  <td class="ten__mon">' 
+    +tenmon
+    +' </td>  <td class="so__luong"> <input type="text" name="name-field"> </td>  <td class="gia__mon">'
+    + giamon+ '</td> '
+    + '<td> <button  onclick="deleteMeal()"> Delete </button> </td> </tr>   ';
+    document.getElementById('bang_hoa_don').insertAdjacentHTML('afterend', html);
 }
-function createBillFunction(){
-    alert("Tạo mới hóa đơn");
-}
+ 
